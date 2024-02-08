79d67bb37fdd727fd3c05a3ee60fb08ce2b839fa
hku2002 <hku2002@hanmail.net>
2024-02-08 20:03:18 +0900

    feat: print 샘플 최초 추가

---
+++ practice01/print_sample.py
@@ -0,0 +1,18 @@
+# 기본 출력법
+print('sample01')
+print("sample02")
+print()
+print('''sample03''')
+print("""sample04""")
+
+print()
+
+# separator
+print('A', 'B', 'C', 'D', 'E')
+print('A', 'B', 'C', 'D', 'E', sep=',')
+print('A', 'B', 'C', 'D', 'E', sep='  ')
+print('A', 'B', 'C', 'D', 'E', sep='')
+
+print()
+
+
