ca9fa0eaf12f009d0a40d100b3c73ad2a3d872c3
hku2002 <hku2002@hanmail.net>
2024-02-13 22:00:38 +0900

    feat: id 확인

--- practice02/variable_sample.py
+++ practice02/variable_sample.py
@@ -8,12 +8,14 @@ print(type(n))
 # 동시 선언
 x = y = z = 700
 print(x, y, z)
+print()
 
 # 선언 및 재선언
 var = 75
 var = 'str'
 print(var)  # str
 print(type(var))  # class 'str'
+print()
 
 # Object References
 # 변수 할당 상태
@@ -24,3 +26,19 @@ print(type(var))  # class 'str'
 # 예)
 print(300)
 print(int(300))
+print()
+
+# id 확인
+m = 800
+n = 655
+
+print(id(m))
+print(id(n))
+print(id(m) == id(n))
+print()
+
+a = 800
+b = 800
+print(id(a))
+print(id(b))
+print(id(a) == id(b))
