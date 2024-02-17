5ad52817a03231f95582f7db6d5c526b5c5d7d7a
hku2002 <hku2002@hanmail.net>
2024-02-17 13:33:33 +0900

    feat: 문자열

---
+++ practice03/str_sample.py
@@ -0,0 +1,16 @@
+# 문자형
+
+str1 = "Hello World!"
+str2 = 'Hello World!'
+str3 = """Hello World!"""
+str4 = '''Hello World!'''
+
+print(type(str1), type(str2), type(str3), type(str4))
+print(len(str1))
+print()
+
+str_empty1 = ''
+str_empty2 = str()
+
+print(type(str_empty1), len(str_empty1))
+print(type(str_empty2), len(str_empty2))
