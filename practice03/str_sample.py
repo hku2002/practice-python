5487db6c4ec66b8df9c003e0cb0ec8f1c290984e
hku2002 <hku2002@hanmail.net>
2024-02-19 21:03:56 +0900

    feat: 멀티라인 입력 추가

--- practice03/str_sample.py
+++ practice03/str_sample.py
@@ -19,3 +19,18 @@ print()
 print("I'm sample!")
 print('I\'m sample!')
 print('Hello \t World')
+
+# 멀티라인 입력
+multi_str1 = """
+문자열
+멀티
+입니다.
+"""
+multi_str2 = \
+    """
+    문자열
+    멀티
+    입니다.
+    """
+print(multi_str1)
+print(multi_str2)
