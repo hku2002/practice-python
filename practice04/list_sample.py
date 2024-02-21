90e87fb403f0b87d368135cfe72b723be37a5d5c
hku2002 <hku2002@hanmail.net>
2024-02-21 21:38:55 +0900

    feat: list  최초 추가

---
+++ practice04/list_sample.py
@@ -0,0 +1,13 @@
+# 리스트
+# 순서 존재, 중복 가능, 수정 가능, 삭제 가능
+
+# 선언
+a = []
+b = list()
+c = [0, 1, 2, 3]
+d = [1000, 2000, 'sample1', 'sample2', True, False]
+e = [1, 2, [1000, 2000], ['sample1', 'sample2'], [True, False]]
+print(c)
+print(len(c))
+print(d)
+print(e)
