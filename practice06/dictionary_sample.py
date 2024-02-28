0eb3ff993a0a3f9de749fbc45c1eddeadef8afbf
hku2002 <hku2002@hanmail.net>
2024-02-28 21:14:14 +0900

    feat: 딕셔너리 추가

--- practice06/dictionary_sample.py
+++ practice06/dictionary_sample.py
@@ -5,3 +5,20 @@
 a = {}
 b = {'key1': 'value1', 'key2': 'value2'}
 print(a, b)
+
+c = {'key1': [1, 2, 3, 4], 'key2': 'value2'}
+print(c)
+
+d = {
+    'Name': 'Gil Dong',
+    'Age': 33,
+    'Gender': 'Male',
+    'Activated': True
+}
+print(d)
+
+e = dict([
+    ('Name', 'Gil Dong'),
+    ('Age', 33)
+])
+print(e)
